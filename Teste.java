5b4550a7f477ebeef8f277653c45910362404bec
kkreis <kaiquesilvariosdosreis@gmail.com>
2026-09-09 01:46:01 -0300

    1.3

--- Teste.java
+++ Teste.java
@@ -4,6 +4,20 @@ public class Teste {
     public static void main(String[] args) {
         Scanner ler = new Scanner(System.in);
         boolean senhaCorreta = false;
+        boolean usuarioAutenticado = false;
+
+        while (!usuarioAutenticado) {
+            System.out.print("Digite o nome de usuário: ");
+            String usuario = ler.next();
+
+
+            if (usuario.equals("kkreis")) {
+                System.out.println("Usuário autenticado.");
+                usuarioAutenticado = true;
+            } else {
+                System.out.println("Credenciais incorretas. Tente novamente.");
+            }
+        }
 
         while (!senhaCorreta) {
             System.out.print("Digite a senha: ");
@@ -16,13 +30,10 @@ public class Teste {
                 System.out.println("Senha incorreta. Tente novamente.");
             }
         }
-
-        // Pede um número, salva na variável e depois mostra o valor
-        System.out.print("Digite um número: ");
-        int numero = ler.nextInt();
-        ler.nextLine(); // limpa a linha do buffer
+        ler.nextLine();// limpa a linha do buffer deixada pelo nextInt()
+        // Pede um nome, salva na variável e depois mostra o valor
         System.out.print("seu nome é?: ");
-        String nome = ler.nextLine();
+        String nomeUsuario = ler.nextLine();
         System.out.print("digite sua idade: ");
         int idade = ler.nextInt();
         ler.nextLine(); // limpa a linha do buffer deixada pelo nextInt()
@@ -33,7 +44,7 @@ public class Teste {
             return;
         }
 
-        System.out.println("Suas informaçoes sao: " + "seu nome é " + nome + " tem " + idade + " anos " + "seu numero é " + numero);
+        System.out.println("Ola, " + nomeUsuario + " Seja bem-vindo.");
 
         // informaçoes do usuario
         System.out.println("qual informação você deseja acessar? ");
@@ -41,13 +52,56 @@ public class Teste {
         if (informacao.equals("idade")) {
             System.out.println("Você acessou a informação: " + idade);
         } else if (informacao.equals("nome")) {
-            System.out.println("Você acessou a informação: " + nome);
-        } else if (informacao.equals("numero")) {
-            System.out.println("Você acessou a informação: " + numero);
+            System.out.println("Você acessou a informação: " + nomeUsuario);
+
         } else {
             System.out.println("Informação inválida.");
         }
 
-        ler.close();
+        
+        System.out.println("qual tipo de operação: ");
+        String operacao = ler.nextLine();
+
+        if (operacao.equals("adicao")) {
+            System.out.println("Digite o primeiro número: ");
+            int num1 = ler.nextInt();
+            System.out.println("Digite o segundo número: ");
+            int num2 = ler.nextInt();
+            int resultado = num1 + num2;
+            System.out.println("O resultado da adição é: " + resultado);
+
+        } else if (operacao.equals("subtracao")) { // if else é para varias condiçoes 
+            System.out.println("Digite o primeiro número: ");
+            int nums1 = ler.nextInt();
+            System.out.println("Digite o segundo número: ");
+            int nums2 = ler.nextInt();
+            int resultado = nums1 - nums2;
+            System.out.println("O resultado da subtração é: " + resultado);
+
+        } else if (operacao.equals("multiplicacao")) {
+            System.out.println("Digite o primeiro número: ");
+            int numm1 = ler.nextInt();
+            System.out.println("Digite o segundo número: ");
+            int numm2 = ler.nextInt();
+            int resultado = numm1 * numm2;
+            System.out.println("O resultado da multiplicação é: " + resultado);
+
+        } else if (operacao.equals("divisao")) {
+            System.out.println("Digite o primeiro número: ");
+            int numd1 = ler.nextInt();
+            System.out.println("Digite o segundo número: ");
+            int numd2 = ler.nextInt();
+            if (numd2 == 0) {
+                System.out.println("Não é possível dividir por zero.");
+            } else {
+                double resultado = (double) numd1 / numd2;
+                System.out.println("O resultado da divisão é: " + resultado);
+            }
+
+        } else {
+            System.out.println("Operação inválida.");
+
+            ler.close();
+        }
     }
 }
